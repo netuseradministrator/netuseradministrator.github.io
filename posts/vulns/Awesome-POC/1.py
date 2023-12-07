d4ffc2e993d676743c8dd2221b2ffcc4f313a7c7
netuseradministrator <1505115532@qq.com>
2023-12-08 01:32:30 +0800

    更新

---
+++ posts/vulns/Awesome-POC/1.py
@@ -0,0 +1,34 @@
+import os
+import re
+
+def add_header(filename):
+    with open(filename, 'r', encoding='utf-8') as f:
+        content = f.read()
+
+    # 获取文件名
+    filename = os.path.basename(filename)
+
+    # 创建头部
+    header = """
++++
+title = '{title}'
+date = '2023-12-08T16:08:29+08:00'
+draft = true
+categories= ["漏洞知识库"]
+tags = ["漏洞知识库"]
++++
+""".format(title=filename[:-3])
+
+    # 将头部添加到内容中
+    content = header + content
+
+    # 将内容写入文件
+    with open(filename, 'w', encoding='utf-8') as f:
+        f.write(content)
+
+if __name__ == '__main__':
+    # 获取目录中的所有 MD 文件，包括子目录
+    for root, dirs, files in os.walk('.'):
+        for file in files:
+            if file.endswith('.md'):
+                add_header(os.path.join(root, file))
